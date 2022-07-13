c36473786519fe2a6362df8e6bc1d7bbfc3dc0f1
Alan Rebane <rebane.alan@gmail.com>
2022-07-13 08:54:48 +0200

    adding tests

--- cms.rb
+++ cms.rb
@@ -2,11 +2,29 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
 root = File.expand_path("..", __FILE__)
 
 get '/' do
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end
-  erb :index
+  erb :index, layout: :layout
+end
+
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+  puts file_path
+
+  if File.file?(file_path)
+    headers["Content-Type"] = "text/plain"
+    File.read(file_path)
+  else
+    session[:error] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
 end

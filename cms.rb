39ddc1bed3e74568ce2d1e531eaac34b8e1c4445
Alan Rebane <rebane.alan@gmail.com>
2022-07-14 18:49:09 +0200

    enabling file editing

--- cms.rb
+++ cms.rb
@@ -8,35 +8,40 @@ configure do
   set :session_secret, 'secret'
 end
 
-helpers do
-  def render_markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    markdown.render(text)
-  end
-
-  def load_file_content(path)
-    content = File.read(path)
-    case File.extname(path)
-    when ".txt"
-      headers["Content-Type"] = "text/plain"
-      content
-    when ".md"
-      render_markdown(content)
-    end
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
   end
 end
 
-root = File.expand_path("..", __FILE__)
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    render_markdown(content)
+  end
+end
 
 get '/' do
-  @files = Dir.glob(root + "/data/*").map do |path|
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
     File.basename(path)
   end
   erb :index, layout: :layout
 end
 
 get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   if File.file?(file_path)
     load_file_content(file_path)
@@ -47,25 +52,19 @@ get "/:filename" do
 end
 
 get "/:filename/edit" do
-  @file_name = params[:filename]
-  file_path = root + "/data/" + @file_name
+  file_path = File.join(data_path, params[:filename])
 
-  if File.file?(file_path)
-    @content = File.read(file_path)
-  else
-    session[:message] = "#{params[:filename]} does not exist."
-    redirect "/"
-  end
+  @filename = params[:filename]
+  @content = File.read(file_path)
 
-  erb :edit, layout: :layout
+  erb :edit
 end
 
-post "/:filename/edit" do
-  @file_name = params[:filename]
-  file_path = root + "/data/" + @file_name
+post "/:filename" do
+  file_path = File.join(data_path, params[:filename])
 
-  File.open(file_path, "w") { |f| f.write params[:file_content] }
+  File.write(file_path, params[:content])
 
-  session[:message] = "#{@file_name} has been updated"
+  session[:message] = "#{params[:filename]} has been updated."
   redirect "/"
 end
